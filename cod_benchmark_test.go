184f2d0e01a8420f2f282aeec0d39a420fe19fa8
TillK17 <tikurek@t-online.de>
2025-03-20 22:27:06 +0100

    fix

--- cod_benchmark_test.go
+++ cod_benchmark_test.go
@@ -71,17 +71,17 @@ func Benchmark_CanadianOpenData(b *testing.B) {
 	log.Printf("Selected %d queries", len(queries))
 	// Run benchmark
 	for _, numH := range numHash {
-		precision := make([]float64, len(thresholds))
-		recall := make([]float64, len(thresholds))
-		f1 := make([]float64, len(thresholds))
-		MinHashDomainTime := make([]float64, len(thresholds))
-		MinHashDomainSpace := make([]float64, len(thresholds))
-		MinHashQueryTime := make([]float64, len(thresholds))
-		MinHashQuerySpace := make([]float64, len(thresholds))
-		BuildIndexTime := make([]float64, len(thresholds))
-		BuildIndexSpace := make([]float64, len(thresholds))
-		QueryIndexTime := make([]float64, len(thresholds))
-		QueryIndexSpace := make([]float64, len(thresholds))
+		precision := []float64{}
+		recall := []float64{}
+		f1 := []float64{}
+		MinHashDomainTime := []float64{}
+		MinHashDomainSpace := []float64{}
+		MinHashQueryTime := []float64{}
+		MinHashQuerySpace := []float64{}
+		BuildIndexTime := []float64{}
+		BuildIndexSpace := []float64{}
+		QueryIndexTime := []float64{}
+		QueryIndexSpace := []float64{}
 
 		for _, threshold := range thresholds {
 			log.Printf("Canadian Open Data benchmark with %d Hash permutations and threshold = %.2f", numH, threshold)
@@ -102,7 +102,7 @@ func Benchmark_CanadianOpenData(b *testing.B) {
 			BuildIndexSpace = append(BuildIndexSpace, s[2])
 			QueryIndexTime = append(QueryIndexTime, t[3])
 			QueryIndexSpace = append(QueryIndexSpace, s[3])
-			//runtime.GC()
+			runtime.GC()
 			os.Remove(linearscanOutput)
 			os.Remove(lshensembleOutput)
 		}
