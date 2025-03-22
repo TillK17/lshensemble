457a6ec4a3e1036ed2fa5409908de51b129e5287
TillK17 <tikurek@t-online.de>
2025-03-22 15:27:59 +0100

    mm3-32-small

--- cod_benchmark_test.go
+++ cod_benchmark_test.go
@@ -107,7 +107,7 @@ func Benchmark_CanadianOpenData(b *testing.B) {
 			os.Remove(lshensembleOutput)
 		}
 		// Output results
-		accuracy_filename := fmt.Sprintf("_cod_accuracy_FNV_32_%d_small.csv", numH)
+		accuracy_filename := fmt.Sprintf("_cod_accuracy_mm3_32_%d_small.csv", numH)
 		file, err := os.Create(accuracy_filename)
 		if err != nil {
 			panic(err)
@@ -128,7 +128,7 @@ func Benchmark_CanadianOpenData(b *testing.B) {
 		file.Close()
 		log.Printf("Accuracy report output to %s", accuracy_filename)
 
-		performance_filename := fmt.Sprintf("_cod_performance_FNV_32_%d_small.csv", numH)
+		performance_filename := fmt.Sprintf("_cod_performance_mm3_32_%d_small.csv", numH)
 		file, err = os.Create(performance_filename)
 		if err != nil {
 			panic(err)
